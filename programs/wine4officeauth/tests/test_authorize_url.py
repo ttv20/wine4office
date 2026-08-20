8e8adac1f985e59f09a28c238207d397111bb17d
ttv20 <ttv20@noreply.codeberg.org>
2026-08-20 16:01:00 +0300

    wine4officeauth: Cover browser creation ordering

--- programs/wine4officeauth/tests/test_authorize_url.py
+++ programs/wine4officeauth/tests/test_authorize_url.py
@@ -10,11 +10,14 @@ class AuthorizeUrlSourceTest(unittest.TestCase):
         build = source.index('authorize_url = "https://login.microsoftonline.com/"')
         convert = source.index("authorize_url_w = utf8_to_wide(authorize_url);", build)
         validate = source.index("if (authorize_url_w.empty()) return false;", convert)
+        create_browser = source.index("CoCreateInstance(CLSID_WebBrowser", validate)
         allocate = source.index("url = SysAllocString(authorize_url_w.c_str());", validate)
         navigate = source.index("hr = browser->Navigate(url,", allocate)
 
         self.assertLess(build, convert)
         self.assertLess(convert, validate)
+        self.assertLess(validate, create_browser)
+        self.assertLess(create_browser, allocate)
         self.assertLess(validate, allocate)
         self.assertLess(allocate, navigate)
 
